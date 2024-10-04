ab7c643c92801fa38b425ae80a5872b7a3f7d316
tai-jp <horiitaiga@gmail.com>
2024-10-04 06:20:37 +0000

    10月4日写経

--- 10_4/main.js
+++ 10_4/main.js
@@ -14,4 +14,13 @@ function errorInput(){
 
 }
 
-
+const monthSelect = document.getElementById("month2");
+monthSelectList();
+function monthSelectList(){
+    for(let i=1;i<=12;i++){
+    let option = document.createElement('option');
+    option.value = i;
+    option.textContent = i + "月";
+    monthSelect.appendChild(option);
+    }
+}
